b93ac527b232a498a7da27d4ddd21ebe090f28ad
lipemecprec <lipemecprec@yahoo.com.br>
2021-03-28 20:31:41 -0300

    Hello world

---
+++ my_app/01.Hello_world.rb
@@ -0,0 +1,16 @@
+#A veru small hello world
+
+# require "roda"
+
+# Pizza = Struct.new(:flavor)
+# class App < Roda
+#     plugin :h
+
+#     mystery_guest = Pizza.new("Mozzarella")
+    
+#     route do |r|
+#         r.get "mystery_guest" do
+#             "<h1>The mystery guest is: #{h mystery_guest}!</h1>"
+#         end
+#     end
+# end
