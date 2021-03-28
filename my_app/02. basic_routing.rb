869b2e66c34c637784438aa6f1776e8d02edfc22
lipemecprec <lipemecprec@yahoo.com.br>
2021-03-28 20:32:07 -0300

    basic routing

---
+++ my_app/02. basic_routing.rb
@@ -0,0 +1,14 @@
+# # Basic routing
+# require "roda"
+
+# class App < Roda
+#     route do |r|
+#         name = "Lucid"
+#         r.on "hello" do
+#             "Hello, #{name}!"
+#         end
+#         r.on "goodbye" do
+#             "Farewell, #{name}!"
+#         end
+#     end
+# end
