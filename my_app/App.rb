1f5763c9d76abb07ade23f3711001c0105136001
lipemecprec <lipemecprec@yahoo.com.br>
2021-04-24 18:33:40 -0300

    public plugin

--- my_app/App.rb
+++ my_app/App.rb
@@ -2,23 +2,56 @@
 require 'roda'
 
 class App < Roda
+  plugin :public
+  
+  ARTICLES = [
+    "This is an article",
+    "This is another article",
+    "This is a post",
+    "And this is whatever you want it to be",
+  ]
 
-  route do |r|  
-    r.get ['israel', {all: ['posts', Integer]}] do |id|
-        # GET /post matches as
-        # GET /posts/1 matches as
-        # GET /posts/new does not match as
-      "<h1>#{id} é o cara</h1>"
+  models = ["account", "post"]
+  route do |r|
+
+    r.root do
+      r.redirect "/account/", 303
+    end
+
+    r.on "public" do
+      r.public
+    end
+    
+    r.on models do |model_name|
+      model_class = Object.const_get(model_name.capitalize)
+      
+      r.get "index" do 
+        model_class.all.join(" | ")
+      end
+
+      r.on Integer do |id|
+        model = model_class[id]
+        
+        r.get "show" do
+          model.to_s
+        end
+
+        r.post "update" do
+          "Updating #{model}"
+        end
+      end
+    end
+
+    r.post "articles" do
+      ARTICLES << r.params["content"]
+      "Latest: #{ARTICLES.last} | Count: #{ARTICLES.count}"
+    end
+
+    r.on "search" do
+      # r.params.inspect
+      ARTICLES.filter do |article|
+        article.include?(r.params["q"].to_s)
+      end.join(" | ")
     end
   end
-
-  # route do |r|  
-  #   r.on "posts", method: :post do |id|
-  #       # GET /post matches as
-  #       # GET /posts/1 matches as
-  #       # GET /posts/new does not match as
-  #     "#{id}"
-  #   end
-  # end
-
 end
